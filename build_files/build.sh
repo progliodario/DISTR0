f10c4318eda1d7df97880ae82dddb7d4f32940c7
progliodario <proglio.dario.dal.2008@gmail.com>
2026-06-22 23:56:59 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,9 @@
 
 set -ouex pipefail
 
-### Install packages
+# Install packages
+dnf5 install -y tmux
+dnf5 install -y edge
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
+# Enabling services
 systemctl enable podman.socket
