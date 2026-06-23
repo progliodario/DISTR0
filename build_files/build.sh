5841cd48371306722a433d51caaee645437917b5
progliodario <proglio.dario.dal.2008@gmail.com>
2026-06-23 13:09:47 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,6 +3,12 @@
 set -ouex pipefail
 
 dnf5 remove -y \
-  firefox.x86_64 
+  firefox.x86_64
+  
+curl -sSL -O https://packages.microsoft.com/fedora/44/prod/Packages/p/packages-microsoft-prod-1.1-3.noarch.rpm
+rpm -i packages-microsoft-prod-1.1-3.noarch.rpm
+rm packages-microsoft-prod-1.1-3.noarch.rpm
+dnf5 update
+dnf5 install microsoft-edge.stable
 
 systemctl enable podman.socket
