eadf761e2269f917850572f79eb8ce508ea8dd86
progliodario <proglio.dario.dal.2008@gmail.com>
2026-06-23 12:42:33 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,7 +3,6 @@
 set -ouex pipefail
 
 dnf5 remove -y firefox.x86_64
-dnf5 remove -y gnome-contacts.x86_64
-dnf5 install -y microsoft-edge-stable
+dnf5 install -y microsoft-edge
 
 systemctl enable podman.socket
