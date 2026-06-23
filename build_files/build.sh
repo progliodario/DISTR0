24bc7e84725752213f78f857fe33972afa6147fa
progliodario <proglio.dario.dal.2008@gmail.com>
2026-06-23 11:36:33 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,8 +5,8 @@ set -ouex pipefail
 # Installing packages
 dnf5 install -y tmux
 
-flatpak install - y flathub com.microsoft.Edge
-flatpak uninstall - y flathub org.mozilla.firefox
+flatpak install -y flathub com.microsoft.Edge
+flatpak uninstall -y flathub org.mozilla.firefox
 
 # Enabling services
 systemctl enable podman.socket
