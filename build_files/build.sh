3ab0bcf0e1276d7f0155f3e8e6d6acd95492532c
progliodario <proglio.dario.dal.2008@gmail.com>
2026-06-23 13:32:10 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,8 +5,8 @@ set -ouex pipefail
 dnf5 remove -y \
   firefox.x86_64
   
-curl -sSL -O https://packages.microsoft.com/fedora/44/prod/Packages/p/packages-microsoft-prod-1.1-3.noarch.rpm
-rpm -i -y packages-microsoft-prod-1.1-3.noarch.rpm
+sudo curl -sSL -O https://packages.microsoft.com/fedora/44/prod/Packages/p/packages-microsoft-prod-1.1-3.noarch.rpm
+rpm -i packages-microsoft-prod-1.1-3.noarch.rpm
 rm packages-microsoft-prod-1.1-3.noarch.rpm
 dnf5 update
 dnf5 install -y microsoft-edge.stable
