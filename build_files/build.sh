69fd656e9d952fce266d54c88e4e99d404eccedb
progliodario <proglio.dario.dal.2008@gmail.com>
2026-06-23 13:46:49 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,9 @@ set -ouex pipefail
 
 dnf5 remove -y \
   firefox.x86_64
-  
+
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-modify --enable flathub
 sudo flatpak install flathub com.microsoft.Edge
 
 systemctl enable podman.socket
