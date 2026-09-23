801b580879058b7dd8a36ed6e0348057654c6a74
progliodario <proglio.dario.dal.2008@gmail.com>
2026-09-23 06:48:10 +0200

    Remove Microsoft Edge installation from build script
    Removed installation steps for Microsoft Edge from the build script.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,17 +2,6 @@
 
 set -ouex pipefail
 
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-cat > /etc/yum.repos.d/microsoft-edge.repo << 'REPOEOF'
-[microsoft-edge]
-name=microsoft-edge
-baseurl=https://packages.microsoft.com/yumrepos/edge
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-REPOEOF
-dnf install -y microsoft-edge-stable
-
 #dnf5 remove -y \
   
 
